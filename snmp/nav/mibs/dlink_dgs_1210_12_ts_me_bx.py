dec2649514a67d5641cb7f941e10f617fc18d51c
Alexander Fefelov <alexanderfefelov@yandex.ru>
2020-09-16 17:17:50 +0300

    Implement D-Link system PoE sensors

--- snmp/nav/mibs/dlink_dgs_1210_12_ts_me_bx.py
+++ snmp/nav/mibs/dlink_dgs_1210_12_ts_me_bx.py
@@ -1,6 +1,13 @@
+import inspect
 from nav.mibs.dlink_dgs_1210_xx import _DLink_DGS_1210_XX_Mib
 from nav.smidumps import get_mib
 
 
 class DLink_DGS_1210_12_TS_ME_BX_Mib(_DLink_DGS_1210_XX_Mib):
     mib = get_mib('D_Link_DGS_1210_12TSME_BX_7_03_001_mib')
+
+    def _get_system_poe_sensors(self):
+        self._logger.debug(here(self))
+        return []
+
+here = lambda this : '{}:{} {}.{}'.format(inspect.stack()[1].filename, inspect.stack()[1].lineno, type(this).__name__, inspect.stack()[1].function)
--- snmp/nav/mibs/dlink_dgs_1210_xx.py
+++ snmp/nav/mibs/dlink_dgs_1210_xx.py
@@ -28,8 +28,10 @@ class _DLink_DGS_1210_XX_Mib(MibRetriever):
         self._logger.debug(here(self))
         ddm_columns = yield self._get_ddm_columns()
         ddm_sensors = self._get_ddm_sensors(ddm_columns)
+        system_poe_sensors = self._get_system_poe_sensors()
         result = []
         result.extend(ddm_sensors)
+        result.extend(system_poe_sensors)
         defer.returnValue(result)
 
     def _get_ddm_columns(self):
@@ -56,6 +58,32 @@ class _DLink_DGS_1210_XX_Mib(MibRetriever):
             result.append(self._get_port_sensor(port, 'ddmTemperature', Sensor.UNIT_CELSIUS))
             result.append(self._get_port_sensor(port, 'ddmBiasCurrent', Sensor.UNIT_AMPERES, Sensor.SCALE_MILLI))
         return result
+
+    def _get_system_poe_sensors(self):
+        self._logger.debug(here(self))
+        result = []
+        result.append(self._get_sensor('pethPsePortPowerBudget', Sensor.UNIT_WATTS))
+        result.append(self._get_sensor('pethPsePortPowerConsumption', Sensor.UNIT_WATTS))
+        result.append(self._get_sensor('pethPsePortPowerRemainder', Sensor.UNIT_WATTS))
+        result.append(self._get_sensor('pethPsePortPowerRatioOfSystemPower', Sensor.UNIT_PERCENT))
+        return result
+
+    def _get_sensor(self, sensor, unit_of_measurement):
+        self._logger.debug(here(self))
+        module_name = self.get_module_name()
+        oid = str(self.nodes[sensor].oid) + '.0'
+        internal_name = sensor
+        description = internal_name
+        return dict(
+            mib=module_name,
+            oid=oid,
+            name=internal_name,
+            internal_name=internal_name,
+            description=description,
+            unit_of_measurement=unit_of_measurement,
+            precision=0,
+            scale=None
+        )
 
     def _get_port_sensor(self, port, sensor, unit_of_measurement, scale=None):
         self._logger.debug(here(self))
