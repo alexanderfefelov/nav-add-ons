630e32e212da7365750e72f9ef3c5712dcb8a34d
Alexander Fefelov <alexanderfefelov@yandex.ru>
2020-09-22 14:48:42 +0300

    Haste makes waste

--- snmp/nav/mibs/dlink_equipment.py
+++ snmp/nav/mibs/dlink_equipment.py
@@ -6,6 +6,6 @@ from nav.smidumps import get_mib
 class DLink_Equipment_Mib(MibRetriever, DLink):
     mib = get_mib('D_Link_Equipment_mib')
     ROOT_OID = 'swEquipmentMIB'
-    GET_FAN_SENSORS = '_get_fan_sensors_old'
-    GET_POWER_SENSORS = '_get_power_sensors_old'
-    GET_TEMPERATURE_SENSORS = '_get_temperature_sensors_old'
+    GET_FAN_SENSORS = 'get_fan_sensors_old'
+    GET_POWER_SENSORS = 'get_power_sensors_old'
+    GET_TEMPERATURE_SENSORS = 'get_temperature_sensors_old'
