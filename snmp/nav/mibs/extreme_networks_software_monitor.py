bd4e812db4d0001aca1cc10140392e2bb299017c
Alexander Fefelov <alexanderfefelov@yandex.ru>
2021-03-16 21:29:47 +0300

    Collect metrics for Extreme Networks CPU

---
+++ snmp/nav/mibs/extreme_networks_software_monitor.py
@@ -0,0 +1,37 @@
+from nav.mibs.mibretriever import MibRetriever
+from nav.mibs.snmp_add_on import SnmpAddOn
+from nav.smidumps import get_mib
+from twisted.internet import defer
+import inspect
+
+
+class Extreme_Networks_Software_Monitor_Mib(MibRetriever, SnmpAddOn):
+    mib = get_mib('Extreme_Networks_EXTREME_SOFTWARE_MONITOR_MIB_mib')
+    ROOT_OID = 'extremeSwMonitor'
+
+    @defer.inlineCallbacks
+    def get_cpu_loadavg(self):
+        self._logger.debug(here(self))
+        result = {}
+        is_supported = yield self.is_oid_supported(self.ROOT_OID)
+        if is_supported:
+            columns = yield self.retrieve_columns([
+                'extremeCpuMonitorSystemSlotId',
+                'extremeCpuMonitorSystemUtilization1min',
+                'extremeCpuMonitorSystemUtilization5mins'
+            ])
+            if columns:
+                for _, item in columns.items():
+                    slot_id = item.get('extremeCpuMonitorSystemSlotId')
+                    result[str(slot_id)] = [
+                        (1, item.get('extremeCpuMonitorSystemUtilization1min')),
+                        (5, item.get('extremeCpuMonitorSystemUtilization5mins'))
+                    ]
+        defer.returnValue(result)
+
+    def get_cpu_utilization(self):
+        self._logger.debug(here(self))
+        return defer.succeed(None)
+
+
+here = lambda this: 'here: {}:{} {}.{}'.format(inspect.stack()[1].filename, inspect.stack()[1].lineno, type(this).__name__, inspect.stack()[1].function)
