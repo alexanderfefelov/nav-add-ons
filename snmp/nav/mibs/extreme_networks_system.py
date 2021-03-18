38124dce817f4d9a6a6f04c23e3d168d38214153
Alexander Fefelov <alexanderfefelov@yandex.ru>
2021-03-18 12:06:14 +0300

    Some work on #8

---
+++ snmp/nav/mibs/extreme_networks_system.py
@@ -0,0 +1,36 @@
+from nav.mibs.mibretriever import MibRetriever
+from nav.mibs.snmp_add_on import SnmpAddOn
+from nav.smidumps import get_mib
+from twisted.internet import defer
+import inspect
+
+
+class Extreme_Networks_System_Mib(MibRetriever, SnmpAddOn):
+    mib = get_mib('Extreme_Networks_EXTREME_SYSTEM_MIB_mib')
+    ROOT_OID = 'extremeSystem'
+
+    @defer.inlineCallbacks
+    def get_all_sensors(self):
+        self._logger.debug(here(self))
+        result = []
+        is_supported = yield self.is_oid_supported(self.ROOT_OID)
+        if is_supported:
+            fan_sensors = yield self._get_fan_sensors()
+            result.extend(fan_sensors)
+        else:
+            self._logger.warning('%s is not supported', self.ROOT_OID)
+        self._logger.info('%d sensor(s) detected', len(result))
+        defer.returnValue(result)
+
+    def _get_fan_sensors(self):
+        self._logger.debug(here(self))
+        result = []
+        is_supported = yield self.is_oid_supported(self.ROOT_OID)
+        if is_supported:
+            pass
+        else:
+            self._logger.warning('%s is not supported', self.ROOT_OID)
+        defer.returnValue(result)
+
+
+here = lambda this: 'here: {}:{} {}.{}'.format(inspect.stack()[1].filename, inspect.stack()[1].lineno, type(this).__name__, inspect.stack()[1].function)
