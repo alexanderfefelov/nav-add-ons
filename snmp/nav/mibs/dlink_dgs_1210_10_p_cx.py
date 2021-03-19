7c27e1c4ddde20beac56ed8bd3c6d89bf5c5b6a1
Alexander Fefelov <alexanderfefelov@yandex.ru>
2021-03-19 06:42:21 +0300

    Haste makes waste

--- snmp/nav/mibs/dlink_dgs_1210_10_p_cx.py
+++ snmp/nav/mibs/dlink_dgs_1210_10_p_cx.py
@@ -6,5 +6,5 @@ from nav.smidumps import get_mib
 class DLink_DGS_1210_10_P_CX_Mib(MibRetriever, DLink_DGS_1210_XX):
     mib = get_mib('D_Link_DGS_1210_10P_CX_4_10_002_mib')
     ROOT_OID = 'dgs-1210-10pcx'
-    GET_PORTS_POE_SENSORS = 'ports_poe_sensors'
-    GET_SYSTEM_POE_SENSORS = 'system_poe_sensors'
+    GET_PORTS_POE_SENSORS = 'get_ports_poe_sensors'
+    GET_SYSTEM_POE_SENSORS = 'get_system_poe_sensors'
