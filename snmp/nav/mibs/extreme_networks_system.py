5bb0f92836f87e539d5faa7258de242c0112dd61
Alexander Fefelov <alexanderfefelov@yandex.ru>
2021-03-20 08:52:26 +0300

    Implement Extreme Networks fan sensors

--- snmp/nav/mibs/extreme_networks_system.py
+++ snmp/nav/mibs/extreme_networks_system.py
@@ -1,5 +1,6 @@
 from nav.mibs.mibretriever import MibRetriever
 from nav.mibs.snmp_add_on import SnmpAddOn
+from nav.models.manage import Sensor
 from nav.smidumps import get_mib
 from twisted.internet import defer
 import inspect
@@ -18,10 +19,21 @@ class Extreme_Networks_System_Mib(MibRetriever, SnmpAddOn):
         self._logger.info('%d sensor(s) detected', len(result))
         defer.returnValue(result)
 
+    @defer.inlineCallbacks
     def _get_fan_sensors(self):
         self._logger.debug(here(self))
         result = []
-        return result
+        columns = yield self.retrieve_columns([
+            'extremeFanNumber',
+            'extremeFanOperational',
+            'extremeFanSpeed'
+        ])
+        if columns:
+            for _, item in columns.items():
+                index = item.get('extremeFanNumber')
+                result.append(self.get_indexed_system_sensor(index, 'extremeFanOperational', ''))
+                result.append(self.get_indexed_system_sensor(index, 'extremeFanSpeed', Sensor.UNIT_RPM))
+        defer.returnValue(result)
 
 
 here = lambda this: 'here: {}:{} {}.{}'.format(inspect.stack()[1].filename, inspect.stack()[1].lineno, type(this).__name__, inspect.stack()[1].function)
